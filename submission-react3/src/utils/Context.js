c92fd92eab18adbdaa67225bfbc5653051c06b85
Suyud Setiawan Al Arif <suyudsetiawan@gmail.com>
2023-02-19 08:17:07 +0800

    delete directory

--- submission-react3/src/utils/Context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export const LocaleContent = createContext();
-
-export const ContextProvider = LocaleContent.Provider;
-export const ContextConsumer = LocaleContent.Consumer;
